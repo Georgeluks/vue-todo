ed16c2f6df1d3d3adaa173c56f8f60ef2c2b59f6
George Lucas <george.lucasebertz98@gmail.com>
2024-07-03 11:10:48 -0300

    init commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <main>
+      <h1>Sobre este Projeto</h1>
+      <p>
+        Este trabalho foi feito utilizando o framework VueJS para o trabalho de
+        GB da cadeira de Desenvolvimento Web e Aplicativos.
+      </p>
+    </main>
+  </div>
+</template>
+
+<style>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+
+  p {
+    font-size: large;
+  }
+}
+</style>
